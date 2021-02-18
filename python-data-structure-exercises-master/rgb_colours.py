a6020ec1ba7685de4e3190e2fb15e58643176553
SAM <samabiodun09@gmail.com>
2021-02-18 14:59:57 +0100

    solved rgb_colours.py

--- python-data-structure-exercises-master/rgb_colours.py
+++ python-data-structure-exercises-master/rgb_colours.py
@@ -14,18 +14,40 @@
 # $ python rgb_colours.py "burnt sienna"
 # I don't know the RGB code for burnt sienna
 
+
+import argparse
+parser = argparse.ArgumentParser() 
+parser.add_argument("color", help="pass in station", nargs="?")
+args = parser.parse_args()
+
+
+
 colours = [
-    ['red', 'F00'],
-    ['yellow', 'FF0'],
-    ['green', '0F0'],
-    ['cyan', '0FF'],
-    ['blue', '00F'],
-    ['magenta', 'F0F'],
+     ['red', 'F00'],
+['yellow', 'FF0'],
+ ['green', '0F0'],
+ ['cyan', '0FF'],
+  ['blue', '00F'],
+  ['magenta', 'F0F'],
 ]
 
 
 # TODO:
 # * Implement the program as described in the comments at the top of the file.
+for color, code in colours:
+    if color == args.color.lower():
+        ans = "The RGB code for {} is {}".format(color, code)
+        print(ans)
+    
+    if  code ==args.color.upper():
+        ans = "The color name for {} is {}".format( code, color)
+        print(ans)
+
+
+
+
+
+#
 
 # TODO (extra):
 # * Change the program so that users can also enter an RGB colour code, and be
